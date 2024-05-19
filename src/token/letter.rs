66b78144c6381dbd1762c1eb55c705cffed7cd02
= <rayyankhan1428h@gmail.com>
2024-05-19 05:10:59 -0400

    Assimilation update

--- src/token/letter.rs
+++ src/token/letter.rs
@@ -38,17 +38,11 @@ pub enum Letter {
 	Yah
 }
 
-/// If the letter is a sun letter, then Al will blend with the first letter of the world. Naar (fire) with The
-/// would become An-naar.
-/// If the letter is a moon letter, then Baab (door) with The would become Al-Baab.
-pub enum Consonant {
-	Sun,
-	Moon
-}
-
 impl Letter {
-	/// This controls the method the is blended with this.
-	pub fn get_consonant(&self) -> Consonant {
+	/// If the letter is a sun letter, then Al will blend with the first letter of the world. Naar (fire) with The
+	/// would become An-naar.
+	/// If the letter is a moon letter, then Baab (door) with The would become Al-Baab.
+	pub fn is_assimilating(&self) -> Consonant {
 		match self {
 			Self::Alif
 			| Self::Baa
